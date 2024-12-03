bd0cd409d0a62dcd8ada806f5293b1e487ffbcd1
Mubashir <mubashir@192.168.10.3>
2024-12-04 01:09:25 +0500

    Day asssigmen task

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,12 +1,29 @@
- export default {
+export default {
     template: `
-      <button class="bg-blue-500 ml-2 text-white px-6 py-2 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300" :disabled="processing">
-        <slot/>
+      <button 
+        :class="{
+          'focus:outline-none focus:ring-2 focus:ring-blue-300': true,
+          'bg-blue-500 ml-2 text-white px-6 py-2 rounded-lg hover:bg-blue-700': type === 'primary',
+          'bg-gray-500 ml-2 text-white px-6 py-2 rounded-lg hover:bg-gray-700': type === 'secondary',
+          'bg-gray-200 ml-2 text-gray-500 px-6 py-2 rounded-lg hover:bg-gray-300': type === 'muted',
+          'is-loading': processing,
+        }" 
+        :disabled="processing"
+      >
+        <slot></slot>
       </button>
     `,
-    data() {
-        return{
-            processing : true
-        }
+    
+    props: {
+      type: {
+        type: String,
+        default: 'primary' // Default type is 'primary'
+      },
+      processing: {
+        type: Boolean,
+        default: false,
     }
-  }
+    },
+   
+  };
+  
