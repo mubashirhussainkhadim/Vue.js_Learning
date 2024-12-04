7716558302104b3d092720b85f1afbb99cd185e8
Mubashir <mubashir@192.168.10.3>
2024-12-04 17:16:43 +0500

    Vue js

--- js/components/App.js
+++ js/components/App.js
@@ -1,6 +1,9 @@
-import AppButton from  "./AppButton.js";
-export default ({
-    components: {
-      "app-button": AppButton
-    },
-  });
+import Assignments from "./Assignments.js";
+
+export default {
+    components: { Assignments },
+
+    template: `
+        <assignments></assignments>    
+    `,
+}
